8752051483cd9ef02d7d4ab185992edbf15da30a
vishalbasnet23 <vishalbasnet23@gmail.com>
2017-05-16 11:55:01 +0545

    component division

---
+++ vuesearch/src/components/ResultModal.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="result-modal">
+    {{message}}
+  </div>
+</template>
+<script>
+  export default {
+    data: () => ({
+      message: 'This is result Modal'
+    })
+  }
+</script>
---
+++ vuesearch/src/components/SearchResults.vue
@@ -0,0 +1,17 @@
+<template>
+  <div id="search-results">
+    {{message}}
+    <result-modal></result-modal>
+  </div>
+</template>
+<script>
+  import ResultModal from './ResultModal.vue';
+  export default {
+    data: () => ({
+      message: 'This is search results'
+    }),
+    components: {
+      'result-modal': ResultModal
+    }
+  }
+</script>
